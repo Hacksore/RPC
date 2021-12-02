a7c12139dd96eb14966c71789665346096226c9a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2021-12-02 16:47:40 -0600

    Export events/commands

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const util = require("./util");
 
+const constants = require("./constants");
+
 module.exports = {
   Client: require("./client"),
   register(id) {
     return util.register(`discord-${id}`);
   },
+  Commands: constants.RPCCommands,
+  Events: constants.RPCEvents,
 };
